143926103112edf1adc5061211a92c5bb5a6bfcd
DaviCancar <97752609+DaviCancar@users.noreply.github.com>
2022-01-14 14:05:51 -0300

    Add files via upload

---
+++ aula davi prog.js
@@ -0,0 +1,22 @@
+var cubo = createSprite(200, 200,20,20);
+
+
+
+function draw(){
+
+    background("yellow");
+
+    if(keyDown("right"))
+       { background("red");}
+    
+          
+
+
+ drawSprites();
+
+    }
+
+  
+    
+     
+
